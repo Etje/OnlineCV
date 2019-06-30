aa475eac01e5cf46c21552c8ab0cbe54d579d58d
etje <etien_ruhl@live.nl>
2019-06-30 13:13:34 +0200

    add margin to portfolio items

--- page-portfolio.php
+++ page-portfolio.php
@@ -15,6 +15,8 @@ get_header();
 	</div>
 </header>
 <section id="portfolio-items">
+    <div class="container portfolio-item">
+      <div class="row align-items-center">
 
     <?php $loop = new WP_Query( array('post_type' => 'portfolio_items', 'orderby' => 'post_id', 'order' => 'ASC', 'post_status' => 'publish'))?>
     <?php while($loop->have_posts() ) : $loop->the_post(); 
@@ -35,9 +37,7 @@ get_header();
 
     ?>
 
-    <div class="container portfolio-item">
-      <div class="row align-items-center">
-        <div class="col-lg-12 order-lg-1">
+        <div class="col-lg-12 order-lg-1 my-20">
             <div class="p-5">
                 <div class="portfolio-title">
                     <h2 class="text-center id-subtitle"><?= the_field('portfolio_projectnaam'); ?></h2>
@@ -57,20 +57,22 @@ get_header();
                         <?php
                     }
                     ?>
+                </div>
             </div>
+            <?php endwhile; ?>
         </div>
-      </div>
     </div>
 
-    <?php endwhile; ?>
 
-    <div class="container text-center">
-        <h2>Hobbymatig heb ik ook een aantal projecten gedaan. Onderstaand staat een overzichtje hiervan!</h2>
+    <div class="container text-center bg-black text-white">
+        <h3>Hobbymatig heb ik ook een aantal projecten gedaan. Onderstaand staat een overzichtje hiervan!</h3>
     </div>
-
-    <?php $loop = new WP_Query( array('post_type' => 'eigen_projecten', 'orderby' => 'post_id', 'order' => 'ASC', 'post_status' => 'publish'))?>
-    <?php while($loop->have_posts() ) : $loop->the_post(); 
+    <div class="container portfolio-item">
+      <div class="row align-items-center">
     
+    <?php $loop = new WP_Query( array('post_type' => 'eigen_projecten', 'orderby' => 'post_id', 'order' => 'ASC', 'post_status' => 'publish'))?>
+    <?php while($loop->have_posts() ) : $loop->the_post();
+
     $image = get_field('portfolio_afbeelding');
     $link = get_field('portfolio_link_naam');
 
@@ -87,9 +89,7 @@ get_header();
 
     ?>
 
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-12 order-lg-1">
+        <div class="col-lg-12 order-lg-1 my-4">
             <div class="p-5">
                 <div class="portfolio-title">
                     <h2 class="text-center id-subtitle"><?= the_field('portfolio_projectnaam'); ?></h2>
@@ -113,10 +113,10 @@ get_header();
                 </div>
             </div>
         </div>
+        <?php endwhile; ?>
       </div>
     </div>
 
-    <?php endwhile; ?>
 
     <div class="portfolio-image"></div>
   </section>
